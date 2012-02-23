94e970721be2fd019f78492ef2874a5ca1ebccb7
Yohan G <yohan@giarelli.org>
2012-02-23 20:00:29 +0100

    Updated sourcepath management

--- lib/fwLessphp.class.php
+++ lib/fwLessphp.class.php
@@ -45,7 +45,7 @@ class fwLessphp
         // compile it
         $destinationFile = preg_replace($this->sourcePattern, $this->destinationPattern, $stylesheet);
         $lessCompiler = new lessc;
-        $lessCompiler->importDir = sfConfig::get('sf_root_dir').sfConfig::get('fw_lessphp_source_base_path', '/data/less');
+        $lessCompiler->importDir = dirname($this->getSourcePath($stylesheet));
 
         // Write it if necessary
         if ($write) {
